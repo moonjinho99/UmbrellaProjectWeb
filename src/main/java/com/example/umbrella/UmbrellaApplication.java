586abd3310742fd54ca901eb7e29816c3421847e
quequuen <rkdusl2020@gmail.com>
2024-01-17 16:17:52 +0900

    초초초기설정

---
+++ src/main/java/com/example/umbrella/UmbrellaApplication.java
@@ -0,0 +1,14 @@
+package com.example.umbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/umbrella/UmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
