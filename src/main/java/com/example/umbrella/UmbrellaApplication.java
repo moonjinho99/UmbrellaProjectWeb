d9576747341de917bd883ed740c1461f2381facc
gustlr7374 <gustlr3127@gmail.com>
2024-01-30 16:39:16 +0900

    20240130 로그인 회원가입

--- src/main/java/com/example/umbrella/UmbrellaApplication.java
+++ src/main/java/com/example/umbrella/UmbrellaApplication.java
@@ -10,5 +10,4 @@ public class UmbrellaApplication {
 		SpringApplication.run(UmbrellaApplication.class, args);
 
 	}
-
 }
--- src/main/java/com/example/umbrella/controller/MemberController.java
+++ src/main/java/com/example/umbrella/controller/MemberController.java
@@ -7,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
@@ -25,5 +25,56 @@ public class MemberController {
         System.out.print("멤버 리스트 : "+memberDtoList);
         return "member/login";
     }
+
+    // 로그인
+    @PostMapping(value = "/login-user", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String loginUser(@RequestBody MemberDto memberDto){
+
+        System.out.println("넘어온 값 : "+ memberDto);
+
+        if(memberDto.getId().equals("qwer") && memberDto.getPw().equals("1234"))
+        {
+            return "success";
+        }
+
+        else{
+            return "fail";
+        }
+    }
+
+    // 아이디 중복확인
+    @PostMapping(value = "/id-check", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String idCheck(@RequestBody MemberDto memberDto){
+
+        if(!(memberDto.getId().equals("qwer")))
+        {
+            return "success";
+        }
+
+        else{
+            return "fail";
+        }
+
+    }
+
+    // 회원가입
+    @PostMapping(value = "/join-user", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String joinUser(@RequestBody MemberDto memberDto){
+
+        if(memberDto.getId().equals("qqqq") && memberDto.getName().equals("방현식") && memberDto.getPw().equals("1234") && memberDto.getPhone().equals("01076565796"))
+        {
+            return "success";
+        }
+
+        else{
+            return "fail";
+        }
+
+    }
+
+
 
 }
