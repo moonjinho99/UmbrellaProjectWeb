624979a120fe97e7478fcf71116d993ee13471e1
quequuen <rkdusl2020@gmail.com>
2024-02-05 23:02:17 +0900

    main 수정

--- src/main/java/com/example/umbrella/controller/CenterController.java
+++ src/main/java/com/example/umbrella/controller/CenterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ public class CenterController {
         return "main";
     }
 
+    @ResponseBody
     @PostMapping(value = "/getAllCenter")
     public List<CenterDto> getAllCenterToAjax(){
         List<CenterDto> centerList = new ArrayList<CenterDto>();
